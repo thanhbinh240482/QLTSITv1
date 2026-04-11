43534161fe66ba3863614fbde61142596f3c8540
thanhbinh240482 <59075429+thanhbinh240482@users.noreply.github.com>
2026-04-11 16:20:29 +0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 5173,
-        allowedHosts: 'all',
+        allowedHosts: true,
         proxy: {
             '/api': {
                 target: 'http://backend:3001',
